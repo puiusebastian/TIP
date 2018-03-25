3a9344ad2ab773fb8d89d4d319389412e7063a44
puiusebastian <puiusebastian@gmail.com>
2018-03-25 12:00:28 +0300

    Created Web Services project

---
+++ TanksWarWebServices/src/webservices/MyApplication.java
@@ -0,0 +1,11 @@
+package webservices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+// adnotarea ApplicationPath specifica calea URL spre serviciile web
+// e.g., @ApplicationPath("rest") => http://localhost:8080/ProiectServiciiWeb/rest/
+@ApplicationPath("rest")
+public class MyApplication extends Application {
+
+}
