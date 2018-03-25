8420ddfa29ae74e4b07c2ee68295d1ec7df90a4f
puiusebastian <puiusebastian@gmail.com>
2018-03-25 11:39:20 +0300

    Created Web Client

---
+++ TanksWarClient/src/test/ClientTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.net.URI;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
+
+public class ClientTest {
+	public static void main(String[] args) {
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
+
+		WebTarget target = client.target(getBaseURI());
+
+		// rest  -> numele specificat in fisierul web.xml de la server in interiorul tag-ului url-pattern
+		// hello -> numele specificat in fisierul Hello.java la adnotarea @Path
+		System.out.println(target.path("rest").path("hello").request()
+			.accept(MediaType.TEXT_PLAIN).get(Response.class).toString());
+
+		System.out.println(target.path("rest").path("hello").request()
+			.accept(MediaType.TEXT_PLAIN).get(String.class));
+
+		System.out.println(target.path("rest").path("hello").request()
+			.accept(MediaType.TEXT_XML).get(String.class));
+
+		System.out.println(target.path("rest").path("hello").request()
+			.accept(MediaType.TEXT_HTML).get(String.class));
+	}
+
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost:8888/").build();
+	}
+}
