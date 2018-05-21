aed4957fa8e70feae93825ea6cb2706e1b76eb12
ilie05 <ilie05@users.noreply.github.com>
2018-05-21 22:31:36 +0300

    Insert into and delete from tank_picked finally implemented

--- TanksWarServer/src/servlets/EndGame.java
+++ TanksWarServer/src/servlets/EndGame.java
@@ -1,12 +1,24 @@
 package servlets;
 
+
 import java.io.IOException;
+import java.net.URI;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
+import javax.ws.rs.core.MediaType;
+
+
 
 /**
  * Servlet implementation class EndGame
@@ -30,10 +42,23 @@ public class EndGame extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		HttpSession session = request.getSession();
+
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
 		
+		
+
+		WebTarget target = client.target(getBaseURI());
+		HttpSession session = request.getSession();
 	    
 	    Object username =  request.getSession().getAttribute("user");
+	    
+	    if(target.path("api/ssw/deletetankpicked").path((String) username).request(MediaType.TEXT_PLAIN).accept(MediaType.APPLICATION_JSON).delete(Boolean.class)) {
+			System.out.println("Line in tank_picked was deleted!");
+		}else {
+			System.out.println("Line in tank_picked wasn't deleted!");
+		}
+	    
 	    session.setAttribute( (String) username, null);                   //unset tank attribut on game end 
 		response.sendRedirect("game.jsp");
 		
@@ -46,5 +71,8 @@ public class EndGame extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost:8888/").build();
+	}
 }
--- TanksWarServer/src/servlets/PickTank.java
+++ TanksWarServer/src/servlets/PickTank.java
@@ -1,12 +1,25 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.client.Entity;
+import org.glassfish.jersey.client.ClientConfig;
+
 
 /**
  * Servlet implementation class PickTank
@@ -30,11 +43,25 @@ public class PickTank extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
 		
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
+		
+		
+
+		WebTarget target = client.target(getBaseURI());
 		HttpSession session = request.getSession();
 		
 	    String t=request.getParameter("tank"); 
-	    
 	    Object username =  request.getSession().getAttribute("user");
+	    
+	    JsonObject tpObject=Json.createReader(new StringReader("{\"user\":\""+(String) username+"\",\"tank\":"+Integer.parseInt(t)+"}")).readObject();
+		if(target.path("api/ssw/inserttankpicked").request(MediaType.TEXT_PLAIN).post(Entity.entity(tpObject, MediaType.APPLICATION_JSON),Boolean.class)){
+			System.out.println("Line in tank_picked was inserted!");
+		}
+		else {
+			System.out.println("Line in tank_picked wasn't inserted!");
+		}
+	    
 	    session.setAttribute( (String) username, t);                   //use username to create a session ----  (user, tank_picked)
 		//setting session to expiry in 30 mins
 		session.setMaxInactiveInterval(30*60);
@@ -48,5 +75,8 @@ public class PickTank extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost:8888/").build();
+	}
 }
